b388014faddd8e1bee134aa1250166bcbc2c9163
schejken <jenka@akol.se>
2015-03-06 16:33:37 +0100

    Added Routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize']);
+var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/',
+            {
+                templateUrl: 'templates/EventDetails.html',
+                controller: 'EventController'
+            })
+            .when('/newEvent',
+            {
+                templateUrl: 'templates/NewEvent.html',
+                controller: 'EditEventController'
+            }
+        )
+    });
