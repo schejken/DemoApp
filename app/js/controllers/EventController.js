bf64d34c80e6f73caceea0033f153cbd5cfe721d
schejken <jenka@akol.se>
2015-03-06 12:11:51 +0100

    Checkin

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,46 +4,18 @@
 'use strict';
 
 eventsApp.controller("EventController",
-    function EventController($scope){
+    function EventController($scope, eventData){
 
         $scope.sortorder = "name";
-        $scope.event = {
-            name: 'Angular Boot Camp',
-            date: '1/1/2015',
-            time: '11:00',
-            location: {
-                address: 'Google Headquaters',
-                city: 'Mountain View',
-                province: 'CA'
+        eventData.getEvent().then(
+            function (event) {
+                $scope.event = event;
             },
-            imageUrl: '/img/profile.jpg',
-            sessions: [
-                {
-                    name: "Directives masterclass",
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'Lorem lups bla bla bla',
-                    upVoteCount: '0'
-                },
-                {
-                    name: "Scopes for fun and profit",
-                    creatorName: 'John Doe',
-                    duration: 2,
-                    level: 'Intro',
-                    abstract: 'Lorem lups bla bla bla',
-                    upVoteCount: '0'
-                },
-                {
-                    name: "Well behaved controllers",
-                    creatorName: 'Jane Doe',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Lorem lups bla bla bla',
-                    upVoteCount: '0'
-                }
-            ]
-        }
+            function (statusCode) {
+                console.log(statusCode);
+            }
+        );
+
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
--- app/js/services/eventData.js
+++ app/js/services/eventData.js
@@ -1,48 +1,20 @@
-'use strict';
-
-eventsApp.factory('eventData', function (eventResource, authService) {
-    return {
-        getEvent: function(eventId, callback) {
-            return eventResource.get({id:eventId}, function(event) {
-                if (callback)
-                    callback(event);
-            });
-        },
-        getAllEvents: function(callback) {
-            return eventResource.queryAll(callback);
-        },
-        getNextSessionId:function (event) {
-            var max = 0;
-            for (var idx = 0; idx < event.sessions.length; idx++) {
-                if (event.sessions[idx].id > max) {
-                    max = event.sessions[idx].id;
-                }
-            }
-            return max+1;
-        },
-        save: function(event, callback) {
-            if (event.id) {
-                eventResource.save(event, function() { if (callback) callback(); });
-            } else {
-                eventResource.queryAll(function(events) {
-                    event.creator = authService.getCurrentUserName();
-                    event.id = getNextEventId(events);
-                    event.sessions = [];
-                    eventResource.save(event);
-                    if (callback)
-                        callback();
+eventsApp.factory('eventData', function ($http, $log, $q) {
+    return{
+        getEvent: function(){
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/data/event/1'
+            }).
+                success(function(data, status, headers, config){
+                    //$log.info(data, status, headers, config);
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    //$log.warn(data, status, headers, config);
+                    deferred.reject(status);
                 });
-            }
+            return deferred.promise;
         }
     };
-
-    function getNextEventId(events) {
-        var max = 0;
-        for (var idx = 0; idx < events.length; idx++) {
-            if (events[idx].id > max) {
-                max = events[idx].id;
-            }
-        }
-        return max+1;
-    }
 });
